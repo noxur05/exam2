74d920d746e7c9354500974ff248fb2246fffa15
Hi-Tech <noxur05>
2024-07-19 17:10:01 +0500

    1.1.3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize Carousel 1
-    new Splide('#carousel-ads', {
-        arrows: false,
-        pagination: false,
+    new Splide('.splide', {
+        arrows: true,
         gap: "1.5rem",
-        type: 'loop',
-        autoplay: true,
-    }).mount();
-  
-    // Initialize Carousel 2
-    new Splide('#carousel-goods', {
-        arrows: true,
         pagination: false,
         // gap: "1rem",
-        perPage: 6,
+        perPage: 3,
         breakpoints: {
             1200: {
-                perPage: 5
-            },
-            992: {
-                perPage: 4
-            },
-            768: {
                 perPage: 3
             },
-            576: {
+            768: {
                 perPage: 2
             },
             320: {
@@ -34,65 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
         focus: 0,
         omitEnd: true
     }).mount();
-
-    new Splide('#carousel-goods2', {
-        arrows: true,
-        pagination: false,
-        // gap: "1rem",
-        perPage: 6,
-        breakpoints: {
-            1200: {
-                perPage: 5
-            },
-            992: {
-                perPage: 4
-            },
-            768: {
-                perPage: 3
-            },
-            576: {
-                perPage: 2
-            },
-            320: {
-                perPage: 1
-            },
-          },
-        focus: 0,
-        omitEnd: true
-    }).mount();
-
-    new Splide('#carousel-goods3', {
-        arrows: true,
-        pagination: false,
-        // gap: "1rem",
-        perPage: 6,
-        breakpoints: {
-            1200: {
-                perPage: 5
-            },
-            992: {
-                perPage: 4
-            },
-            768: {
-                perPage: 3
-            },
-            576: {
-                perPage: 2
-            },
-            320: {
-                perPage: 1
-            },
-          },
-        focus: 0,
-        omitEnd: true
-    }).mount();
-
-    Fancybox.bind('[data-fancybox="gallery"]', {
-        // Your custom options for a specific gallery
-    });
-
-    
-      
       
   });
   
